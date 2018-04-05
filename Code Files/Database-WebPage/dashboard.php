888a3eaf638daf126245700808ea4259f48d7954
Jackson Neff <neffj@oregonstate.edu>
2018-04-04 19:03:34 -0700

    Homepage v0.1
    Added dummy_data_script.php and gs2styes.css. Moved functions.php and
    updated it. Updated dashboard.php.

--- Code Files/Database-WebPage/dashboard.php
+++ Code Files/Database-WebPage/dashboard.php
@@ -1,28 +1,183 @@
+<!DOCTYPE html>
+<html>
+
 <?php
 include_once 'functions.php';
+include_once 'fusioncharts/fusioncharts.php';
 $conn = gs2_database_connect();
-if($conn){
-    echo "Connection Successful";
-}
-else {
-    echo "Connection to database failed";
-}
+if(!$conn){echo "Connection to database failed";}
 ?>
 
-<html>
+<head>
+    <title>GS2 Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="library/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="library/gs2styles.css"/>
+    <script type="text/javascript" src="fusioncharts/js/fusioncharts.js"></script>
+    <script type="text/javascript" src="fusioncharts/js/fusioncharts.theme.ocean.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+    <nav class="navbar  navbar-dark bg-primary sticky-top" style="margin-bottom: 10px; background-color: #5cb85c;">
+        <a class="navbar-brand" href="#" style="color: white;"><b>My Smart Gardening System</b></a>
+    </nav>
+</head>
+
 <body>
-    <div>Enter a row into the database here:</div>
-    <div>gs2_database: Timeslice table</div>
-    <?php
-        $table = array(array());
-        $name = "Timeslice";
-        $table = get_table($conn, $name);
-    ?>
-    <div> ID: <?php echo $table['0']['0']; ?></div>
-    <div> Timestamp <?php echo $table['0']['1']; ?></div>
-    <div> Temp: <?php echo $table['0']['2']; ?></div>
-    <div> Pressure: <?php echo $table['0']['3']; ?></div>
-    <div> Humidity: <?php echo $table['0']['4']; ?></div>
+
+<?php
+$height = 200;
+
+$table = array(array());
+$name = "Timeslice";
+$table = get_table($conn, $name);
+$tempArray = array();
+$humidityArray = array();
+$pressureArray = array();
+$soilmoistureArray = array();
+$soilphArray = array();
+$batteryArray = array();
+$timeArray = array();
+$timeArray2 = array();
+$num_rows = get_num_rows($conn, $name);
+for ($i = 1; $i <= 10; $i++){
+    $timeArray[$i] = date('H:i:s', strtotime($table[$num_rows - (11 - $i)]['1']));
+    $tempArray[$i] = $table[$num_rows - (11 - $i)]['2'];
+    $humidityArray[$i] = $table[$num_rows - (11 - $i)]['3'];
+    $pressureArray[$i] = $table[$num_rows - (11 - $i)]['4'];
+    $soilmoistureArray[$i] = $table[$num_rows - (11 - $i)]['5'];
+    $soilphArray[$i] = $table[$num_rows - (11 - $i)]['6'];
+    $batteryArray[$i] = $table[$num_rows - (11 - $i)]['7'];
+}
+$temp_time_Array = array_combine($timeArray, $tempArray);
+$humidity_time_Array = array_combine($timeArray, $humidityArray);
+$pressure_time_Array = array_combine($timeArray, $pressureArray);
+$soilmoisture_time_Array = array_combine($timeArray, $soilmoistureArray);
+$soilph_time_Array = array_combine($timeArray, $soilphArray);
+/* `$arrData` is the associative array that is initialized to store the chart attributes. */
+$tempChart = makeNewBarGraph("Temperature", "Last 12 hours", $temp_time_Array, "chart-1", $height);
+$tempChart->render();
+$humidityChart = makeNewBarGraph("Humidity", "Last 12 hours", $humidity_time_Array, "chart-2", $height);
+$humidityChart->render();
+$pressureChart = makeNewBarGraph("Pressure", "Last 12 hours", $pressure_time_Array, "chart-3", $height);
+$pressureChart->render();
+$soilmoistureChart = makeNewBarGraph("Soil Moisture", "Last 12 hours", $soilmoisture_time_Array, "chart-4", $height);
+$soilmoistureChart->render();
+$soilphChart = makeNewBarGraph("Soil pH", "Last 12 hours", $soilph_time_Array, "chart-5", $height);
+$soilphChart->render();
+?>
+<div class = "row">
+    <div class = "col-sm-2">
+        <h1>Welcome, User</h1>
+        Left Column</div>
+    <div class = "col-sm-8">
+        <div class = "status-bar">
+            <div class="status-bar-section">
+                <i class="fas fa-thermometer-full"></i>
+                <?php
+                $current_temp = number_format($tempArray[1], 1);
+                echo html_entity_decode($current_temp . "&deg"); ?>
+                <div class="status-bar-section-right">
+                    <span class="status-bar-units">F</span>
+                </div>
+            </div>
+            <div class="status-bar-section">
+                <i class="fas fa-cloud"></i>
+                <?php echo number_format($humidityArray[1], 1); ?>
+                <div class="status-bar-section-right">
+                    <span class="status-bar-units">100%</span>
+                    <span class="status-bar-units">g/kg</span>
+                </div>
+            </div>
+            <div class="status-bar-section">
+                <i class="fas fa-weight"></i>
+                <?php echo number_format($pressureArray[1], 1); ?>
+                <div class="status-bar-section-right">
+                    <span class="status-bar-units">Pa</span>
+                </div>
+            </div>
+            <div class="status-bar-section">
+                <i class="fas fa-tint"></i>
+                <?php echo number_format($soilmoistureArray[1], 1); ?>
+                <div class="status-bar-section-right">
+                    <span class="status-bar-units">Units</span>
+                </div>
+            </div>
+            <div class="status-bar-section">
+                <i class="fas fa-lightbulb"></i>
+                <?php echo number_format($soilphArray[1], 1); ?>
+                <div class="status-bar-section-right">
+                    <span class="status-bar-units">lm</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class = "col-sm-2">Right Column</div>
+</div>
+<div class = "row">
+    <div class = "col-sm-2">Left Column</div>
+    <div class = "col-sm-8">
+        <div class = "category-wrap">
+            <div class="chart-container" id="chart-1">Temp chart renders here</div>
+            <div class="data-container" style="height: <?php echo $height; ?>px;">
+                <?php
+                echo "Now " . $tempArray[1] . "<br>";
+                echo "Average " . $tempArray[1] . "<br>";
+                echo "Trend " . $tempArray[1] . "<br>";
+                echo "High " . $tempArray[1] . "<br>";
+                echo "Low " . $tempArray[1];
+                ?>
+            </div>
+        </div>
+        <div class = "category-wrap">
+            <div class="chart-container" id="chart-2">Temp chart renders here</div>
+            <div class="data-container" style="height: <?php echo $height; ?>px;">
+                <?php
+                echo "Now " . $tempArray[1] . "<br>";
+                echo "Average " . $tempArray[1] . "<br>";
+                echo "Trend " . $tempArray[1] . "<br>";
+                echo "High " . $tempArray[1] . "<br>";
+                echo "Low " . $tempArray[1];
+                ?>            </div>
+        </div>
+        <div class = "category-wrap">
+            <div class="chart-container" id="chart-3">Temp chart renders here</div>
+            <div class="data-container" style="height: <?php echo $height; ?>px;">
+                <?php
+                echo "Now " . $tempArray[1] . "<br>";
+                echo "Average " . $tempArray[1] . "<br>";
+                echo "Trend " . $tempArray[1] . "<br>";
+                echo "High " . $tempArray[1] . "<br>";
+                echo "Low " . $tempArray[1];
+                ?>            </div>
+        </div>
+        <div class = "category-wrap">
+            <div class="chart-container" id="chart-4">Temp chart renders here</div>
+            <div class="data-container" style="height: <?php echo $height; ?>px;">
+                <?php
+                echo "Now " . $tempArray[1] . "<br>";
+                echo "Average " . $tempArray[1] . "<br>";
+                echo "Trend " . $tempArray[1] . "<br>";
+                echo "High " . $tempArray[1] . "<br>";
+                echo "Low " . $tempArray[1];
+                ?>            </div>
+        </div>
+        <div class = "category-wrap">
+            <div class="chart-container" id="chart-5">Temp chart renders here</div>
+            <div class="data-container" style="height: <?php echo $height; ?>px;">
+                <?php
+                echo "Now " . $tempArray[1] . "<br>";
+                echo "Average " . $tempArray[1] . "<br>";
+                echo "Trend " . $tempArray[1] . "<br>";
+                echo "High " . $tempArray[1] . "<br>";
+                echo "Low " . $tempArray[1];
+                ?>            </div>
+        </div>
+    </div>
+    <div class = "col-sm-2">Right Column</div>
+</div>
+
 
 </body>
-</html>
+
+
+
+
---
+++ Code Files/Database-WebPage/functions.php
@@ -0,0 +1,105 @@
+<?php
+include_once 'fusioncharts/fusioncharts.php';
+
+function gs2_database_connect(){
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'Capstone18');
+    define('DB_NAME', 'gs2_database');
+
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if (!$conn) {
+        die('Could not connect to database' . mysqli_connect_error());
+    }
+    else {
+        return $conn;
+    }
+};
+
+function get_table($conn, $tablename){
+    $count = 0;
+    $query =  "SELECT * FROM $tablename";
+    if($result=mysqli_query($conn, $query)){
+
+        while($favor_row = mysqli_fetch_row($result)) {   //puts table of nearby favors in favor_array
+            for ($i = 0; $i < 8; $i++){
+                $array[$count][$i] = $favor_row[$i];
+            }
+            $count = $count+1;
+        }
+    return $array;
+    }
+    else {
+        return false;
+    }
+};
+
+function get_num_rows($conn, $tablename){
+    $count = 0;
+    $query =  "SELECT * FROM $tablename";
+    if($result=mysqli_query($conn, $query)){
+        while($favor_row = mysqli_fetch_row($result)) {   //puts table of nearby favors in favor_array
+            $count = $count+1;
+        }
+        return $count;
+    }
+    else {
+        return 0;
+    }
+};
+
+function makeNewBarGraph($title, $subtitle, $dataArray, $renderAt, $height){
+    $arrData = array(
+        "chart" => array(
+            "caption"=> $title,
+            "subCaption"=> $subtitle,
+            "enableSmartLabels"=> "0",
+            "showValues"=> "0",
+            "showPercentValues"=> "1",
+            "showLegend"=> "1",
+            "decimals"=> "1",
+            "theme"=> "zune",
+            "chartBottomMargin"=> "20",
+            "labelstep" => "3"
+        )
+    );
+    $arrData['data'] = array();
+    // Iterate through the data in `$actualData` and insert in to the `$arrData` array.
+    foreach ($dataArray as $key => $value) {
+        array_push($arrData['data'],
+            array(
+                'label' => $key,
+                'value' => $value
+            )
+        );
+    }
+    $jsonEncodedData = json_encode($arrData);
+    $tempChart = new FusionCharts("line", $title , "100%", $height, $renderAt, "json", $jsonEncodedData);
+    return $tempChart;
+}
+
+
+/*  2d Array print function
+    <table id="timeslice-table">
+        <tr>
+            <th>ID</th>
+            <th>Time</th>
+            <th>Temperature</th>
+            <th>Humidity</th>
+            <th>Pressure</th>
+            <th>Soil Moisture</th>
+            <th>Soil pH</th>
+            <th>Battery</th>
+        </tr>
+
+    </table>
+        $col = 0;
+        for ($row = 0; $row < get_num_rows($conn, $name); $row++) {
+            echo "<tr>";
+            for ($col = 0; $col < 8; $col++) {
+                    echo "<td>" . $table[$row][$col] . "</td>";
+            }
+            echo "</tr>";
+        }
+ */
+?>
